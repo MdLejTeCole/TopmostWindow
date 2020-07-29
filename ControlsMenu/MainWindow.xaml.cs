54b68457023040ac1769737dd58c6946627b810a
MdLejTeCole <48440272+MdLejTeCole@users.noreply.github.com>
2020-07-29 21:57:36 +0200

    Add

---
+++ ControlsMenu/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KeyboardListener KListener = new KeyboardListener();
+        MiniMenu mini;
+        public Key MiniMenuKey { get; set; } = Key.B;
+        public MainWindow()
+        {
+            KListener.KeyDown += new RawKeyEventHandler(KListener_KeyDown);
+            InitializeComponent();
+        }
+        void KListener_KeyDown(object sender, RawKeyEventArgs args)
+        {
+
+            if (args.Key == MiniMenuKey && (mini == null || mini.Visibility != Visibility.Visible))
+            {
+                mini = new MiniMenu();
+                Mouse.OverrideCursor = Cursors.Cross;
+                mini.Show();
+
+            }
+        }
+
+        void KListener_KeyUp(object sender, RawKeyEventArgs args)
+        {
+            if (args.Key == Key.V && mini.Visibility == Visibility.Hidden)
+            {
+                Mouse.OverrideCursor = Cursors.Arrow;
+                mini.Hide();
+            }
+        }
+    }
+}
---
+++ ControlsMenu/MiniMenu.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControlsMenu
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MiniMenu.xaml
+    /// </summary>
+    public partial class MiniMenu : Window
+    {
+        [DllImport("user32.dll")]
+        static extern void ClipCursor(ref System.Drawing.Rectangle rect);
+
+        [DllImport("user32.dll")]
+        static extern void ClipCursor(IntPtr rect);
+        public MiniMenu()
+        {
+            this.Left = Cursor.Position.X - 140;
+            this.Top = Cursor.Position.Y - 140;
+            System.Drawing.Rectangle r = new System.Drawing.Rectangle(Cursor.Position.X - 70, Cursor.Position.Y - 70, Cursor.Position.X + 70, Cursor.Position.Y + 70);
+            ClipCursor(ref r);
+            InitializeComponent();
+            MiniMenuWindow.Topmost = true;
+        }
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            ClipCursor(IntPtr.Zero);
+            this.Hide();
+        }
+        private static class Cursor
+        {
+            internal struct POINT
+            {
+                public int X;
+                public int Y;
+            }
+
+            [DllImport("user32.dll")]
+            static extern bool GetCursorPos(out POINT lpPoint);
+
+            [DllImport("user32.dll")]
+            static extern bool SetCursorPos(int X, int Y);
+
+            public static POINT Position
+            {
+                get
+                {
+                    GetCursorPos(out POINT point);
+                    return point;
+                }
+                set
+                {
+                    SetCursorPos(value.X, value.Y);
+                }
+            }
+        }      
+    }
+}
+
